0c5b0bf4de43fb0d6ceac241acf23e4533e99344
lapeko <lapeko88@gmail.com>
2023-11-21 21:28:39 +0100

    install react + ts + tailwindcss

---
+++ 3-react-ts-tailwind/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}", // update this path to match the file patterns of your project
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
